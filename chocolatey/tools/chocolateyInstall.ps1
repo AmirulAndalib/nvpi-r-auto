ef3f7db6ac28490ef725a628d0db2573864c509d
AmirulAndalib <AmirulAndalib@users.noreply.github.com>
2026-07-08 01:26:11 +0000

    chore: bump nvpi-r to v7.2.0.0
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $installDir = Join-Path $env:USERPROFILE 'NVPI-R'
 $packageArgs = @{
     packageName   = 'nvpi-r'
     unzipLocation = $installDir
-    url           = 'https://github.com/xHybred/NvidiaProfileInspectorRevamped/releases/download/v7.1.0.0/NVPI-R.zip'
-    checksum      = '42dc611298520a8bdedc209461b35d7baba7fa5829af18eaf6a96f468d966a9c'
+    url           = 'https://github.com/xHybred/NvidiaProfileInspectorRevamped/releases/download/v7.2.0.0/NVPI-R.zip'
+    checksum      = '07d45e7e5bc4e64d1f593e253354bcce5da5e19a508dffe855afe8f596aa8646'
     checksumType  = 'sha256'
 }
 
